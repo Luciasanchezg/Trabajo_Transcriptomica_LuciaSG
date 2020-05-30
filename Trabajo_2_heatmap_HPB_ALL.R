bbae2b5f1e367491946e30294291bfb30cb99ca0
Luciasanchezg <66167801+Luciasanchezg@users.noreply.github.com>
2020-05-30 17:30:08 +0200

    Update Trabajo_2_heatmap_HPB_ALL.R

--- Trabajo_2_heatmap_HPB_ALL.R
+++ Trabajo_2_heatmap_HPB_ALL.R
@@ -21,7 +21,6 @@ library(data.table)
 ## ------------------------------------------------------------------------------------ ##
 ## ------------------------------------- HPB_ALL -------------------------------------- ##
 ## ------------------------------------------------------------------------------------ ##
-
 ## 1. Cargar elementos
 # Se carga el objeto eset
 eset_HPB_ALL <- readRDS(file = "eset_HPB_ALL.rds")
@@ -33,12 +32,15 @@ expression_total_HPB_ALL <- exprs(esetIQR_HPB_ALL)
 # Se cargan la tabla de HPB_ALL con el pvalor y el logFC
 toptableIQR_HPB_ALL <- readRDS(file = "toptableIQR_HPB_ALL.rds")
 
+
+
 ## 2. Filtrado de las sondas de interés de toptableIQR_HPB_ALL
 subgrupo_HPB_ALL_pvalor <- subset(toptableIQR_HPB_ALL, toptableIQR_HPB_ALL$adj.P.Val <= 0.05 
             & (toptableIQR_HPB_ALL$logFC < -1))
 
 subgrupo_HPB_ALL_pvalor
 dim(subgrupo_HPB_ALL_pvalor) # En total, 17 sondas downreguladas para HPB_ALL (con LogFC < -1)
+
 
 
 ## 3. Adicion del simbolo a las sondas 
@@ -53,11 +55,14 @@ toptable.annotated_HPB_ALL <- cbind(subgrupo_HPB_ALL_pvalor,GeneSymbol_HPB_ALL)
 head(toptable.annotated_HPB_ALL)
 
 
+
 ## 4. Eliminación de sondas que no tengan un gen asociado (en GeneSymbol pone NA)
 # nueva_toptable_HPB_ALL <- droplevels(toptable.annotated_HPB_ALL[-which(
 #  toptable.annotated_HPB_ALL$GeneSymbol_HPB_ALL == "NA"), ] )
 # dim(nueva_toptable_HPB_ALL) 
 nueva_toptable_HPB_ALL <- toptable.annotated_HPB_ALL 
+
+
 
 ## 5. Si los genes se repiten, se seleccionan aquellos con un pvalor ajustado mejor
 filas_HPB_ALL_menor_pvalor <- lapply(split(nueva_toptable_HPB_ALL,nueva_toptable_HPB_ALL$GeneSymbol_HPB_ALL),
@@ -65,6 +70,7 @@ filas_HPB_ALL_menor_pvalor <- lapply(split(nueva_toptable_HPB_ALL,nueva_toptable
 df_DEG_menor_pvalor <- rbindlist(filas_HPB_ALL_menor_pvalor)
 df_DEG_menor_pvalor[,Sondas := unlist(lapply(filas_HPB_ALL_menor_pvalor , rownames))]
 dim(df_DEG_menor_pvalor)
+
 
 
 ## 6. Obtención de las intensidades de las sondas filtradas en el paso anterior
@@ -83,11 +89,15 @@ row.names(df_intensidades_HPB_ALL) <- df_DEG_menor_pvalor$GeneSymbol_HPB_ALL
 head(df_intensidades_HPB_ALL)
 dim(df_intensidades_HPB_ALL)
 
+
+
 ## 7. Realización de heatmap
 pheatmap(df_intensidades_HPB_ALL)
+
 
 
 ## 8. Guardar el heatmap en una imagen
 png("Heatmap_HPB_ALL.png", width=500, height=500)
 pheatmap(df_intensidades_HPB_ALL)
 dev.off()
+
